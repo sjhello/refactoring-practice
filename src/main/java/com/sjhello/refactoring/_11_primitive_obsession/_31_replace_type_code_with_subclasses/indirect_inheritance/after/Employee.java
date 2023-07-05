a3043f043d134e7d6ec510ccba14c9eb2b2b4c65
sjhello <sjhello235@gmail.com>
2023-07-06 08:18:57 +0900

    primitive_obsession: 이미 상속이 있는 경우(indirect) after

---
+++ src/main/java/com/sjhello/refactoring/_11_primitive_obsession/_31_replace_type_code_with_subclasses/indirect_inheritance/after/Employee.java
@@ -0,0 +1,38 @@
+package com.sjhello.refactoring._11_primitive_obsession._31_replace_type_code_with_subclasses.indirect_inheritance.after;
+
+import java.util.List;
+
+public class Employee {
+	/**
+	 * 이미 상속구조가 존재하는 경우
+	 * */
+
+	private String name;
+	private EmployeeType type;
+
+	public Employee(String name, String typeValue) {
+		this.name = name;
+		this.type = this.employeeType(typeValue);
+	}
+
+	private EmployeeType employeeType(String typeValue) {
+		return switch (typeValue) {
+			case "engineer" -> new Engineer();
+			case "manager" -> new Manager();
+			case "salesman" -> new Salesman();
+			default -> throw new IllegalArgumentException(typeValue);
+		};
+	}
+
+	public String capitalizedType() {
+		return this.type.caplitalizedType();
+	}
+
+	@Override
+	public String toString() {
+		return "Employee{" +
+			"name='" + name + '\'' +
+			", type='" + type.toString() + '\'' +
+			'}';
+	}
+}
---
+++ src/test/java/com/sjhello/refactoring/_11_primitive_obsession/_31_replace_type_code_with_subclasses/indirect_inheritance/after/EmployeeTest.java
@@ -0,0 +1,19 @@
+package com.sjhello.refactoring._11_primitive_obsession._31_replace_type_code_with_subclasses.indirect_inheritance.after;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EmployeeTest {
+
+	@Test
+	void indirect_employee_test() {
+		assertThat(new FullTimeEmployee("sjhello", "manager").capitalizedType()).isEqualTo("Manager");
+		assertThat(new PartTimeEmployee("sjhello", "salesman").capitalizedType()).isEqualTo("Salesman");
+		assertThat(new PartTimeEmployee("sjhello", "engineer").capitalizedType()).isEqualTo("Engineer");
+		assertThrows(IllegalArgumentException.class, () -> {
+			new FullTimeEmployee("sjhello", "developer").capitalizedType();
+		});
+	}
+}
